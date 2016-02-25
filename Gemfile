1e2c62f975b2451852a63b6e661c99c3f80eb006
hardmarlindart <mardem_lincow@yahoo.com.br>
2016-02-25 11:03:22 -0300

    gitbub

--- Gemfile
+++ Gemfile
@@ -5,10 +5,9 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.4'
 # Use sqlite3 as the database for Active Record
 #gem 'sqlite3'
-gem 'pg'
-
 gem 'pg_search'
-
+gem 'pg'
+gem 'devise'
 
 gem 'will_paginate', '~> 3.0.6'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'pessoas#index'
+  devise_for :users
   resources :pessoas
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
